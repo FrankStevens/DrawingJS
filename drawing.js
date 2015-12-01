746769562a75bec777b3612e99f374b8aee54c23
FrankStevens <14mathman@gmail.com>
2015-12-01 10:46:54 -0700

    Create drawing.js

---
+++ drawing.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+
+function clear (){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function rect (x, y, width, height, fill){
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.fillStyle = fill || "#000000";
+    ctx.fill();
+    ctx.closePath();
+}
+function circ (x, y, radius, fill){
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI*2);
+    ctx.fillStyle = fill || "#000000";
+    ctx.fill();
+    ctx.closePath();
+}
+function txt (x, y, msg, fill, font){
+    ctx.font = font || "16px Arial";
+    ctx.fillStyle = fill || "#000000";
+    ctx.fillText(msg, x, y);
+}
+function img (x, y, source, width, height){
+    var image = document.createElement("IMG");
+    image.setAttribute("src", source);
+    var w = width || image.naturalWidth;
+    var h = height || image.naturalHeight;
+    ctx.drawImage(image, x, y, w, h);
+}
+function line (x1, y1, x2, y2, stroke){
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = stroke || "#000000";
+    ctx.stroke();
+    ctx.closePath();
+}
